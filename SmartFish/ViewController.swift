c250ef5c779bd52eca77f14cbcd52879f0831f9d
Chris Walter <walt50@gmail.com>
2018-09-19 16:22:54 -0400

    first commit.  iOS is set up.  Just figure out how to set up raspberryPI

--- SmartFish/ViewController.swift
+++ SmartFish/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    // MARK: Properties
+    var isOn = false
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        led(state: "OFF")
+        
+        
+    }
+    
+    func led(state:String)
+    {
+        let ref = Database.database().reference()
+        let post: [String: AnyObject] = ["state": state as AnyObject]
+        ref.child("led").setValue(post)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func onButtonTapped(_ sender: UIButton)
+    {
+        if isOn
+        {
+            sender.setTitle("OFF", for: .normal)
+            sender.backgroundColor = UIColor.red
+            led(state: "OFF")
+        } else
+        {
+            sender.setTitle("ON", for: .normal)
+            sender.backgroundColor = #colorLiteral(red: 0.4078431373, green: 0.7333333333, blue: 0.4352941176, alpha: 1)
+            led(state: "ON")
+        }
+        isOn = !isOn
+        
     }
-
-
+    
 }
 
